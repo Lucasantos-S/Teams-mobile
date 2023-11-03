c18c54fdcb26c5359280f2a73f6012752fd2ab81
Lucas Santos <lucassantos.dsilv@gmail.com>
2023-11-03 13:26:50 -0300

    :sparkles: feat: created listEmpty

---
+++ src/components/ListEmpty/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components/native";
+import { Warning } from "phosphor-react-native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Message = styled.Text`
+  text-align: center;
+  margin-top: 20;
+
+  font-size: ${(props) => props.theme.FONT_SIZE.SM}px;
+  font-family: ${(props) => props.theme.FONT_FAMILY.REGULAR};
+  color: ${(props) => props.theme.COLORS.GRAY_300};
+`;
+
+export const WarningIcon = styled(Warning).attrs((props) => ({
+  size: 100,
+  color: props.theme.COLORS.GREEN_700,
+}))``;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Container, Title } from "./styles";
 import { Header } from "@/components/Header";
 import { Highligth } from "@/components/Highligth";
 import { GroupCard } from "@/components/GroupCard";
+import { ListEmpty } from "@/components/ListEmpty";
 
 export function Groups() {
   const [groups, setGroups] = React.useState([] as string[]);
@@ -14,6 +15,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { marginTop: 100 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhum turma cadastrada, que tal cadastrar?" />
+        )}
       />
     );
   }, [groups]);
